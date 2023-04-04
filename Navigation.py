6659bb6ddc34b92b20ddd47284a7f93e21301228
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-04-04 15:48:57 -0700

    Update Navigation.py

--- Navigation.py
+++ Navigation.py
@@ -1,55 +1,56 @@
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
-from spike.control import wait_for_seconds, wait_until, Timer
-from math import *
-def pid(hub, robot, cm, speed):
-    motor = Motor('F')
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 
-    motor.set_degrees_counted(0)
+from spike.control import wait_for_seconds, wait_until, Timer
 
-    #Degrees needed per centimeter * centimers needed = degrees_needed
-    degrees_needed = cm/0.0613888889
-    print(degrees_needed)
-    while abs(motor.get_degrees_counted())<=degrees_needed: 
-        print(motor.get_degrees_counted())
-        GSPK = 1.7 
-        steer = (0-hub.motion_sensor.get_yaw_angle())*GSPK 
-        robot.start(int(steer),speed)
-        wait_for_seconds(0.5)
-    print('CATS')
-    robot.stop()
+from math import *
 
-def turnDegrees(robot, deg):
-    robot.move(round(44*deg/360, 2), 'cm', 100, 30)
+def pid(hub, robot, cm, speed):
+
+    motor = Motor('F')
+    motor.set_degrees_counted(0)
+
+    start_angle = hub.motion_sensor.get_yaw_angle()
+
+    #Degrees needed per centimeter * centimers needed = degrees_needed
+    degrees_needed = abs(cm)/0.0613888889
+
+    print(degrees_needed)
+
+    while abs(motor.get_degrees_counted())<=degrees_needed:
+        GSPK = 1.7
+
+        steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
+
+        robot.start(int(steer),speed)
+
+        wait_for_seconds(0.5)
+
+    robot.stop()
+
+    
+def turnDegrees(robot, deg):
+
+    robot.move(round(44*deg/360, 2), 'cm', 100, 30)
 
 
-hub = PrimeHub()
+hub = PrimeHub()
 hub.motion_sensor.reset_yaw_angle()
 
-robot = MotorPair('F', 'B')
+robot = MotorPair('F', 'B')
 robot.set_default_speed(50)
-robot.set_motor_rotation(22.1, 'cm')
+robot.set_motor_rotation(22.1, 'cm')
 
-flipper = Motor('D')
+flipper = Motor('D')
 flipper.set_stop_action('hold')
-#flipper.run_for_degrees(161, 30)
+flipper.run_for_degrees(161, 30)
 
-pid(hub, robot, 200)
+#Go forward and do TV mission, then come back
+pid(hub, robot, 16.25, 30)
+pid(hub, robot, 15.5, -30)
 
-raise SystemError
+#Turn to Car, Go to car
+turnDegrees(robot, -46)
+pid(hub, robot, 79, 30)
 
-robot.move(20,'cm', 0, 30)
-robot.move(-17,'cm', 0, 30)
-turnDegrees(robot, -38)
 
-hub.motion_sensor.reset_yaw_angle()
 
-robot.move(74,'cm', 0, 30)
-
-'''
-flipper.start(0)
-for i in range(3):
-    motor_pair.move(10, 'cm', 0, 30)
-    motor_pair.move(-10, 'cm', 0, 30)
-print('second wait')
-flipper.stop()
-'''
