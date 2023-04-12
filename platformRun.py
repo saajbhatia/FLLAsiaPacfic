0bd3b8e40bb30bc3ed2c6574ea9110c19512643e
SiddBW09 <112580443+SiddBW09@users.noreply.github.com>
2023-04-11 20:14:21 -0700

    Update platformRun.py
    Pid almost works, platform work 3/4 of the time.

--- platformRun.py
+++ platformRun.py
@@ -5,18 +5,20 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-def pid(hub, robot, cm, speed):
+def pid(hub, robot, cm, speed, start_angle):
     motor = Motor('F')
     motor.set_degrees_counted(0)
-    start_angle = hub.motion_sensor.get_yaw_angle()
+    #start_angle = hub.motion_sensor.get_yaw_angle()
     #Degrees needed per centimeter * centimers needed = degrees_needed
-    degrees_needed = abs(cm) * 360/22.1
+    degrees_needed = abs(cm)/0.0613888889
     print(degrees_needed)
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
+        print(steer)
+        print(hub.motion_sensor.get_yaw_angle)
         robot.start(int(steer),speed)
-        wait_for_seconds(0.5)
+        wait_for_seconds(0.01)
     robot.stop()
 
 def turnDegrees(robot, deg):
@@ -27,20 +29,23 @@ hub.motion_sensor.reset_yaw_angle()
 
 robot = MotorPair('F', 'B')
 robot.set_default_speed(50)
-robot.set_motor_rotation(22.1, 'cm')
+#robot.set_motor_rotation(22.1, 'cm')
 
 flipper = Motor('D')
-flipper.set_stop_action('hold')
+#flipper.set_stop_action('hold')
+back_flipper = Motor('A')
+#back_flipper.set_stop_action('hold')
 
 '''
 DO NOT CHANGE
 '''
 
-#flipper.run_for_degrees(90, 30)
-robot.move(round(44*30/360, 2), 'cm', 100, 15)
-pid(hub, robot, 0.2, 30)
-robot.move(round(44*-30/360, 2), 'cm', 100, 15)
-pid(hub, robot, 42, 30)
+
+#robot.move(round(44*30/360, 2), 'cm', 100, 15)
+flipper.run_for_degrees(165, 90)
+pid(hub, robot, 50, 40, 0)
 for i in range(3):
-    flipper.run_for_degrees(-50, 30)
-    flipper.run_for_degrees(50, 30)
+    flipper.run_for_degrees(-30, 60)
+    flipper.run_for_degrees(30, 60)
+    #back_flipper.run_for_degrees(70, 50)
+    #back_flipper.run_for_degrees(-70, 50)
