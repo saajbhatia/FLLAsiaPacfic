8bb81ffe2a138887aa31255e722b0045a46cd050
saajbhatia <86172528+saajbhatia@users.noreply.github.com>
2023-04-12 18:15:56 -0700

    Update dinoRun.py

--- dinoRun.py
+++ dinoRun.py
@@ -1,38 +1,44 @@
 '''
 DO NOT CHANGE
 '''
+
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-def pid(hub, robot, cm, speed, start_angle):
+def pid(hub, robot, cm, speed):
     motor = Motor('F')
     motor.set_degrees_counted(0)
-    #start_angle = hub.motion_sensor.get_yaw_angle()
+    start_angle = hub.motion_sensor.get_yaw_angle()
     #Degrees needed per centimeter * centimers needed = degrees_needed
-    degrees_needed = abs(cm)/0.0613888889
+    degrees_needed = abs(cm) * 360/22.1
     print(degrees_needed)
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
         robot.start(int(steer),speed)
-        wait_for_seconds(0.5)
+        wait_for_seconds(0.1)
     robot.stop()
+
+def abs_turning(hub, robot, deg, speed):
+    for i in range(5):
+        robot.move(33*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
 
 hub = PrimeHub()
 hub.motion_sensor.reset_yaw_angle()
 
 robot = MotorPair('F', 'B')
-robot.set_default_speed(50)
 robot.set_motor_rotation(22.1, 'cm')
 
 flipper = Motor('D')
 flipper.set_stop_action('hold')
 
+'''
+DO NOT CHANGE
+'''
+
 #Power Plant Mission
 pid(hub, robot, 46, 40, 0)
 flipper.run_for_degrees(-60, 50)
 
-'''
-DO NOT CHANGE
-'''
+
