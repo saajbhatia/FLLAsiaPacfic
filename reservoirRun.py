12be3d00b3a4b0ea98efa4cba11abfd3303ea877
saajbhatia <86172528+saajbhatia@users.noreply.github.com>
2023-04-12 18:14:21 -0700

    Update reservoirRun.py

--- reservoirRun.py
+++ reservoirRun.py
@@ -1,35 +1,37 @@
+'''
+DO NOT CHANGE
+'''
+
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-def pid(hub, robot, cm, speed, start_angle):
+def pid(hub, robot, cm, speed):
     motor = Motor('F')
     motor.set_degrees_counted(0)
-    #start_angle = hub.motion_sensor.get_yaw_angle()
+    start_angle = hub.motion_sensor.get_yaw_angle()
     #Degrees needed per centimeter * centimers needed = degrees_needed
-    degrees_needed = abs(cm)/0.0613888889
-    print("Degrees needed: " + str(degrees_needed))
+    degrees_needed = abs(cm) * 360/22.1
+    print(degrees_needed)
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
-        steer = (start_angle - hub.motion_sensor.get_yaw_angle())*GSPK
-        print("Steer: " + str(steer))
-        print("Degrees counted: " + str(motor.get_degrees_counted))
-        #print(hub.motion_sensor.get_yaw_angle)
+        steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
         robot.start(int(steer),speed)
-        wait_for_seconds(0.01)
+        wait_for_seconds(0.1)
     robot.stop()
 
-def turnDegrees(robot, deg):
-    robot.move(round(deg*44/360, 2), 'cm', 100, 30)
+def abs_turning(hub, robot, deg, speed):
+    for i in range(5):
+        robot.move(33*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
 
 hub = PrimeHub()
 hub.motion_sensor.reset_yaw_angle()
 
 robot = MotorPair('F', 'B')
-robot.set_default_speed(50)
+robot.set_motor_rotation(22.1, 'cm')
 
 flipper = Motor('D')
-back_flipper = Motor('A')
+flipper.set_stop_action('hold')
 
 '''
 DO NOT CHANGE
