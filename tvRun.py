a9681c97a2de5746b239aa62e1fcb10bd1e14a8d
saajbhatia <86172528+saajbhatia@users.noreply.github.com>
2023-04-12 20:05:27 -0700

    Update tvRun.py

--- tvRun.py
+++ tvRun.py
@@ -4,34 +4,45 @@ DO NOT CHANGE
 
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
-from math import *
+import math
+import time
 
-def pid(hub, robot, cm, speed):
+def pid(hub, robot, cm, speed, start_angle):
     motor = Motor('F')
     motor.set_degrees_counted(0)
-    start_angle = hub.motion_sensor.get_yaw_angle()
+    #start_angle = hub.motion_sensor.get_yaw_angle()
     #Degrees needed per centimeter * centimers needed = degrees_needed
     degrees_needed = abs(cm) * 360/22.1
-    print(degrees_needed)
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
         robot.start(int(steer),speed)
-        wait_for_seconds(0.1)
+        #wait_for_seconds(0.1)
     robot.stop()
 
 def abs_turning(hub, robot, deg, speed):
+    startTime = time.time()
+    distOfWheels = 11 * math.pi
+    robot.move(distOfWheels*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
     for i in range(5):
-        robot.move(33*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
+        robot.move(distOfWheels*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, 20)
+        if deg-hub.motion_sensor.get_yaw_angle() == 0:
+            break
+    print('Total time is', time.time()-startTime)
 
-hub = PrimeHub()
-hub.motion_sensor.reset_yaw_angle()
+def __init__():
+    hub = PrimeHub()
+    hub.motion_sensor.reset_yaw_angle()
 
-robot = MotorPair('F', 'B')
-robot.set_motor_rotation(22.1, 'cm')
+    robot = MotorPair('F', 'B')
+    robot.set_motor_rotation(22.1, 'cm')
 
-flipper = Motor('D')
-flipper.set_stop_action('hold')
+    flipper = Motor('D')
+    flipper.set_stop_action('hold')
+
+    return hub, robot, flipper
+
+hub, robot, flipper = __init__()
 
 '''
 DO NOT CHANGE
@@ -64,12 +75,31 @@ def check_angle(ang):
     if hub.motion_sensor.get_yaw_angle() >= ang:
         return
 
-def testing():
-    flipper.run_for_degrees(95, 30)
-    pid(hub, robot, 21, 30)
-    pid(hub, robot, 26, -30)
-    abs_turning(hub, robot, -47, 15)
-    pid(hub, robot, 93, 30)
+def mainMissions():
+    #flipper.run_for_degrees(90, 30)
+    pid(hub, robot, 31, 30, 0)
+    pid(hub, robot, 35, -30, 0)
+    jyrt
+    abs_turning(hub, robot, -47, 20)
+    pid(hub, robot, 93, 30, -47)
+    return
     abs_turning(hub, robot, -10, 10)
-    flipper.run_for_degrees(-95, 30)
-testing()
+    flipper.run_for_degrees(-120, 15)
+    abs_turning(hub, robot, -47, 20)
+    pid(hub, robot, 30, -30)
+    abs_turning(hub, robot, 45, 20)
+    flipper.run_for_degrees(120, 15)
+    pid(hub, robot, 25, 30)
+    for i in range(3):
+        pid(hub, robot, 7, 20)
+        pid(hub, robot, 7, -20)
+        wait_for_seconds(0.5)
+    abs_turning(hub, robot, 155, 20)
+    pid(hub, robot, 60, 80)
+
+print('*****************')
+
+
+currentTime = time.time()
+mainMissions()
+print(time.time()-currentTime)
