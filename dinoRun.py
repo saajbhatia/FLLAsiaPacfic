bcd93692d236210109e2c6e34cb00fd014b2f2f1
Aditigosavi <113860164+Aditigosavi@users.noreply.github.com>
2023-04-14 16:09:16 -0700

    The power plant mission works.

--- dinoRun.py
+++ dinoRun.py
@@ -1,6 +1,6 @@
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
-from math import *
+import math
 import time
 def pid(hub, robot, cm, speed, start_angle):
     motor = Motor('F')
@@ -12,12 +12,18 @@ def pid(hub, robot, cm, speed, start_angle):
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
         robot.start(int(steer),speed)
-        #wait_for_seconds(0.1)
+        wait_for_seconds(0.01)
     robot.stop()
 
 def abs_turning(hub, robot, deg, speed):
+    startTime = time.time()
+    distOfWheels = 11 * math.pi
+    robot.move(distOfWheels*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
     for i in range(5):
-        robot.move(33*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
+        robot.move(distOfWheels*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, 20)
+        if deg-hub.motion_sensor.get_yaw_angle() == 0:
+            break
+    print('Total time is', time.time()-startTime)
 
 hub = PrimeHub()
 hub.motion_sensor.reset_yaw_angle()
@@ -28,13 +34,13 @@ robot.set_default_speed(50)
 flipper = Motor('D')
 back_flipper = Motor('A')
 def mission():
-    pid(hub, robot, 54, 20, 0)
-    flipper.run_for_degrees(90,-80)
+    pid(hub, robot, 54, 50, 0)
+    flipper.run_for_degrees(90,-90)
     abs_turning(hub, robot, 5, 40)
-    pid(hub, robot, 7, 30, 5)
-    flipper.run_for_degrees(90,50)
+    pid(hub, robot, 8, 30, 5)
+    flipper.run_for_degrees(90,20)
     abs_turning(hub, robot, 0, 40)
-    pid(hub, robot, 60, -30, 0)
+    pid(hub, robot, 63, -50, 0)
 
 
 currentTime = time.time()
