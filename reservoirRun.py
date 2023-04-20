8248d7babcf77664c40da3c830248f7f6d16c564
Jeffr3yLi <70670857+Jeffr3yLi@users.noreply.github.com>
2023-04-19 19:09:00 -0700

    Running from right side
    The changed backflip makes it very hard to do it from the left side

--- reservoirRun.py
+++ reservoirRun.py
@@ -1,84 +1,82 @@
+#Importing libraries
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
+import time
 
+#Setting up PID controller
 def pid(hub, robot, cm, speed, start_angle):
     motor = Motor('F')
     motor.set_degrees_counted(0)
-    #start_angle = hub.motion_sensor.get_yaw_angle()
-    #Degrees needed per centimeter * centimers needed = degrees_needed
     degrees_needed = abs(cm) * 360/22.1
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
         robot.start(int(steer),speed)
-        #wait_for_seconds(0.1)
+    print("Robot is off by " + str(hub.motion_sensor.get_yaw_angle() - start_angle) + " degrees.")
     robot.stop()
 
+#Used for absolute turning
+def calDiff(curr, correct):
+    if curr - correct > 180:
+        return correct - (curr - 360)
+    elif curr - correct < -180:
+        return correct - (curr + 360)
+    else:
+        return correct - curr
+
+#Setting up absolute turning function
 def abs_turning(hub, robot, deg, speed):
+    distOfWheels = 38.25
+    calDiff(hub.motion_sensor.get_yaw_angle(), deg)
+    robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, speed)
     for i in range(5):
-        robot.move(33*(deg-hub.motion_sensor.get_yaw_angle())/360, 'cm', 100, speed)
+        robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, 20)
+        if calDiff(hub.motion_sensor.get_yaw_angle(), deg) == 0:
+            break
+    print("Robot is off by " + str(hub.motion_sensor.get_yaw_angle() - deg) + " degrees.")
 
-hub = PrimeHub()
-hub.motion_sensor.reset_yaw_angle()
+#Setting up the motors
+def __init__():
+    hub = PrimeHub()
+    hub.motion_sensor.reset_yaw_angle()
 
-robot = MotorPair('F', 'B')
-robot.set_default_speed(50)
+    robot = MotorPair('F', 'B')
+    robot.set_motor_rotation(17.5, 'cm')
 
-flipper = Motor('D')
-back_flipper = Motor('A')
+    flipper = Motor('D')
+    flipper.set_stop_action('hold')
+
+    back_flipper = Motor('A')
+
+    return hub, robot, flipper, back_flipper
+
+hub, robot, flipper, back_flipper = __init__()
+
+#Used for counting how long the program took to run
+startTime = time.time()
+
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
 
 '''
 DO NOT CHANGE
 '''
+#Start
+pid(hub, robot, 70, -30, 0)
 
-print(" ")
-print(" ")
-print(" ")
-print(" ")
-print(" ")
+abs_turning(hub, robot, 45, 50)
 
+pid(hub, robot, 50, -30, 45)
 
+abs_turning(hub, robot, 138, 30)
 
-print("Start Angle: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be 0")
-print(" ")
-
-pid(hub, robot, 39, -20, 0)
-print("Angle before first turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be 0")
-print(" ")
-
-abs_turning(hub, robot, -50, 5)
-print("Angle after first turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -50")
-print(" ")
-
-pid(hub, robot, 53, -20, -50)
-print("Angle before second turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -50")
-print(" ")
-
-abs_turning(hub, robot, -15, 5)
-print("Angle after second turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -15")
-print(" ")
-
-pid(hub, robot, 13, 20, -15)
-print("Angle before third turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -15")
-print(" ")
-
-abs_turning(hub, robot, -33, 5)
-print("Angle after third turn: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -33")
-print(" ")
-
-pid(hub, robot, 2, 20, -33)
-print("Angle before flick down: " + str(hub.motion_sensor.get_yaw_angle()))
-print("Should be -33")
-print(" ")
-
-wait_for_seconds(1)
+pid(hub, robot, 13.5, 50, 138)
 
 back_flipper.run_for_degrees(90, -60)
+
+#End
+print("Time took to run program was: " + str(time.time()-startTime) + " seconds.")
