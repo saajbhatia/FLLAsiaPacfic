940e5367b9d20fa17006d308bc1b4b149dbcfe01
saajbhatia <86172528+saajbhatia@users.noreply.github.com>
2023-04-19 19:08:02 -0700

    Update tvRun.py

--- tvRun.py
+++ tvRun.py
@@ -8,14 +8,18 @@ import math
 import time
 
 def pid(hub, robot, cm, speed, start_angle):
+    #pid(hub, robot, 100, 45, 45)
     motor = Motor('F')
     motor.set_degrees_counted(0)
     #start_angle = hub.motion_sensor.get_yaw_angle()
     #Degrees needed per centimeter * centimers needed = degrees_needed
     degrees_needed = abs(cm) * 360/17.8
+    
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
         steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
+        if cm == 100:
+            print(motor.get_degrees_counted(), degrees_needed, steer, hub.motion_sensor.get_yaw_angle(), start_angle)
         robot.start(int(steer),speed)
         #wait_for_seconds(0.1)
     robot.stop()
@@ -37,7 +41,7 @@ def abs_turning(hub, robot, deg, speed):
         robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, 20)
         if calDiff(hub.motion_sensor.get_yaw_angle(), deg) == 0:
             break
-    print('Total time is', time.time()-startTime)
+    #print('Total time is', time.time()-startTime)
 
 def __init__():
     hub = PrimeHub()
@@ -72,15 +76,17 @@ pid(hub, robot, 21, 50, 45)
 abs_turning(hub, robot, 69, 45)
 flipper.run_for_degrees(-140, 30)
 abs_turning(hub, robot, 45, 45)
-pid(hub, robot, 28, -50, 45)
+pid(hub, robot, 28, -45, 45)
 abs_turning(hub, robot, 137, 45)
 
 #Windmill
 flipper.run_for_degrees(140, 30)
 pid(hub, robot, 15, 50, 137)
+abs_turning(hub, robot, 137, 45)
 for i in range(3):
-    pid(hub, robot, 11, 30, 137)
-    pid(hub, robot, 11, -30, 137)
+    pid(hub, robot, 11.5, 30, 137)
+    pid(hub, robot, 10.5, -30, 137)
+    wait_for_seconds(0.5)
 
 #Put units on floor
 abs_turning(hub, robot, -44, 45)
@@ -94,3 +100,4 @@ pid(hub, robot, 55, 50, -115)
 abs_turning(hub, robot, 0, 45)
 
 print(time.time()-startTime)
+print('***********')
