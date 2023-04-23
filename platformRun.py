f82e4dbb692513ae57c8fb978c5e780c6c6de5ca
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-04-23 12:47:51 -0700

    Update platformRun.py

--- platformRun.py
+++ platformRun.py
@@ -16,7 +16,8 @@ def pid(hub, robot, cm, speed, start_angle):
     degrees_needed = abs(cm) * 360/17.8
     while abs(motor.get_degrees_counted())<=degrees_needed:
         GSPK = 1.7
-        steer = (start_angle-hub.motion_sensor.get_yaw_angle())*GSPK
+        calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
         robot.start(int(steer),speed)
         #wait_for_seconds(0.1)
     robot.stop()
@@ -62,26 +63,31 @@ DO NOT CHANGE
 startTime = time.time()
 def plat():
     back_flipper.run_for_degrees(35, 60)
-    flipper.run_for_degrees(90, 90)
-    pid(hub, robot, 51, 30, 0)
+    flipper.run_for_degrees(160, 90)
+    pid(hub, robot, 51, 50, 0)
     #flipping platforn run
     for i in range(3):
-        flipper.run_for_degrees(-30, 80)
-        flipper.run_for_degrees(25, 80)
+        flipper.run_for_rotations(-0.1, 80)
+        flipper.run_for_rotations(0.1, 80)
     back_flipper.run_for_degrees(-70, 45)
-    abs_turning(hub, robot, 40, 15)
-    flipper.run_for_degrees(-95, 90)
-    pid(hub, robot, 36, 30, 40)
-    abs_turning(hub, robot, 90, 15)
-    #back flip goes down to collect cubes
+    abs_turning(hub, robot, 38, 50)
+    flipper.run_for_degrees(-120, 90)
+    pid(hub, robot, 36, 50, 38)
+    abs_turning(hub, robot, 90, 50)
+    #back flip goes down to collect cylinders
     back_flipper.run_for_degrees(-100, 50)
-    pid(hub, robot, 30, 30, 90)
+    pid(hub, robot, 30, 50, 90)
     back_flipper.run_for_degrees(60, 50)
-    abs_turning(hub, robot, 187, 15)
+    abs_turning(hub, robot, 180, 50)
     back_flipper.run_for_degrees(-10, 50)
-    pid(hub, robot, 21, -30, -173)
-    pid(hub, robot, 21, 30, -173)
-    flipper.run_for_degrees(90, 90)
+    pid(hub, robot, 21, -50, 180)
+    pid(hub, robot, 21, 50, 180)
+    flipper.run_for_degrees(110, 90)
+    abs_turning(hub, robot, 150, 50)
 
+def test():
+    abs_turning(hub, robot, 180, 50)
+    print('Done')
+    pid(hub, robot, 100, 30, 180)
 plat()
 print(time.time()-startTime)
