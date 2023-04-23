514c8537af5f41f6b4d97162d854f9bbe0f56bb5
Aditigosavi <113860164+Aditigosavi@users.noreply.github.com>
2023-04-23 12:21:30 -0700

    We added factory run to the solar plant run.
    Need to fix some transitions.

--- dinoRun.py
+++ dinoRun.py
@@ -35,44 +35,65 @@ flipper = Motor('D')
 back_flipper = Motor('A')
 def mission():
     
+    flipper.run_for_degrees(18,-50)
+
     #Go to pwr plant OG val 53.75 then 52.5 then 47 and lift bar
-    pid(hub, robot, 53, 30, 0)
+    pid(hub, robot, 66, 30, 0)
     flipper.run_for_degrees(80,-90)
 
     #Turn right 35
-    abs_turning(hub, robot, 34, 10)
-    pid(hub, robot, 13, 15, 34)
+    abs_turning(hub, robot, 35, 10)
+    pid(hub, robot, 15, 15, 34)
 
     #Turn back to 0 b4 pushing thing down then push down
     abs_turning(hub, robot, 0, 10)
     flipper.run_for_degrees(90, 90)
-
+    flipper.run_for_degrees(-5, -75)
     #Go HOME 
-    pid(hub, robot, 75, -30, 0)
-    abs_turning(hub, robot, 45, 10)
-    
-    '''
-    abs_turning(hub, robot, -8, 20)
-    flipper.run_for_degrees(90,-90)
+    pid(hub, robot, 84, -60, 0)
+    abs_turning(hub, robot, 90, 0)
 
-    #Turn back after lifting pwr plant thingy
-    abs_turning(hub, robot, 0, 20)
+    #Tv mission go forward and come back
+    pid(hub, robot, 39, 40, 90)
+    pid(hub, robot, 35, -40, 90)
     
-    #Go forward and push down pwr plant thingy
-    pid(hub, robot, 8, 20, 0)
-    flipper.run_for_degrees(110,20)
-    #abs_turning(hub, robot, 0, 40)
+    #Turn to do factory
+    abs_turning(hub, robot, 45, 60)
+    
+    #Setup
+    flipper.run_for_degrees(-140, 30)
 
-    #Go back and turn 45deg
-    #abs_turning(hub, robot, 20, 0)
-    print(hub.motion_sensor.get_yaw_angle())
-    print("Is that vegan?")
-    pid(hub, robot, 63, -20, 0)
-    abs_turning(hub, robot, 0, 20)
-    return
-    
-    abs_turning(hub, robot, 20, 45)
-    '''
+    #Dump + Grab
+    pid(hub, robot, 75, 50, 45)
+    flipper.run_for_degrees(140, 30)
+    pid(hub, robot, 21, 50, 45)
+
+    #Car
+    abs_turning(hub, robot, 69, 45)
+    flipper.run_for_degrees(-140, 30)
+    abs_turning(hub, robot, 45, 45)
+    pid(hub, robot, 28, -45, 45)
+    abs_turning(hub, robot, 137, 45)
+
+    #Windmill
+    flipper.run_for_degrees(140, 30)
+    pid(hub, robot, 15, 50, 137)
+    abs_turning(hub, robot, 137, 45)
+    for i in range(3):
+        pid(hub, robot, 11.5, 30, 137)
+        pid(hub, robot, 10.5, -30, 137)
+        wait_for_seconds(0.5)
+
+    #Put units on floor
+    abs_turning(hub, robot, -44, 45)
+    flipper.run_for_degrees(-140, 30)
+
+    #Go home
+    abs_turning(hub, robot, -105, 45)
+    pid(hub, robot, 55, 50, -115)
+
+    #Align for dino
+    abs_turning(hub, robot, 0, 45)
 
 currentTime = time.time()
 mission()
