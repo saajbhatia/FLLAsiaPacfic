80dc56f2a4a81df3ea2869cedd8b0301e7a2a76e
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-04-28 18:57:19 -0700

    Added highspeed_pid and made abs_turning faster

--- NavigationSpikeWheels.py
+++ NavigationSpikeWheels.py
@@ -1,7 +1,3 @@
-'''
-DO NOT CHANGE
-'''
-
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
@@ -25,6 +21,30 @@ def pid(hub, robot, cm, speed, start_angle):
         #wait_for_seconds(0.1)
     robot.stop()
 
+def highspeed_pid(hub, robot, cm, speed, start_angle):
+    print('*******************')
+    motor = Motor('F')
+    motor.set_degrees_counted(0)
+    #start_angle = hub.motion_sensor.get_yaw_angle()
+    #Degrees needed per centimeter * centimers needed = degrees_needed
+    degrees_needed = abs(cm) * 360/17.8
+    while abs(motor.get_degrees_counted())<=degrees_needed*0.8:
+        GSPK = 1.7
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
+        if speed < 0:
+            steer *= -1
+        print(steer)
+        robot.start(int(steer),speed)
+    while abs(motor.get_degrees_counted())<=degrees_needed:
+        GSPK = 1.7
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
+        if speed < 0:
+            steer *= -1
+        print(steer)
+        robot.start(int(steer), 30)
+        #wait_for_seconds(0.1)
+    robot.stop()
+
 def calDiff(curr, correct):
     if curr - correct > 180:
         return correct - (curr - 360)
@@ -35,13 +55,14 @@ def calDiff(curr, correct):
 
 def abs_turning(hub, robot, deg, speed):
     startTime = time.time()
-    distOfWheels = 38.25
+    distOfWheels = 38.0
     calDiff(hub.motion_sensor.get_yaw_angle(), deg)
     robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, speed)
     for i in range(5):
-        robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, 20)
         if calDiff(hub.motion_sensor.get_yaw_angle(), deg) == 0:
             break
+        robot.move(distOfWheels*calDiff(hub.motion_sensor.get_yaw_angle(), deg)/360, 'cm', 100, 20)
+        
     print('Total time is', time.time()-startTime)
 
 def __init__():
@@ -57,12 +78,10 @@ def __init__():
     back_flipper = Motor('A')
     return hub, robot, flipper, back_flipper
 
-hub, robot, flipper, back_flipper = __init__()
 def waitUntilTap(hub):
     while True:
         if hub.motion_sensor.wait_for_new_gesture() == 'tapped':
             break
 
-'''
-DO NOT CHANGE
-'''
+
+
