42376ad6e8453c83ef01d90a1e49ae5ec7fed0a9
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-05-05 19:58:46 -0700

    Update highspeed_pid

--- NavigationSpikeWheels.py
+++ NavigationSpikeWheels.py
@@ -23,8 +23,45 @@ def pid(hub, robot, cm, speed, start_angle):
         robot.start(int(steer),speed)
         #wait_for_seconds(0.1)
     robot.stop()
-
 def highspeed_pid(hub, robot, cm, speed, start_angle):
+    print('*******************')
+    motor = Motor('F')
+    motor.set_degrees_counted(0)
+    #start_angle = hub.motion_sensor.get_yaw_angle()
+    #Degrees needed per centimeter * centimers needed = degrees_needed
+    degrees_needed = abs(cm) * 360/17.8
+    while abs(motor.get_degrees_counted())<=degrees_needed*0.2:
+        GSPK = 1.7
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
+        if speed < 0:
+            steer *= -1
+        if speed < 0:
+            robot.start(int(steer), -30)
+        else:
+            robot.start(int(steer), 30)
+        #print(steer)
+        robot.start(int(steer),speed)
+    while abs(motor.get_degrees_counted())<=degrees_needed*0.8:
+        GSPK = 1.7
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
+        if speed < 0:
+            steer *= -1
+        #print(steer)
+        robot.start(int(steer),speed)
+    while abs(motor.get_degrees_counted())<=degrees_needed:
+        GSPK = 1.7
+        steer = calDiff(hub.motion_sensor.get_yaw_angle(), start_angle)*GSPK
+        if speed < 0:
+            steer *= -1
+        print(steer)
+        if speed < 0:
+            robot.start(int(steer), -30)
+        else:
+            robot.start(int(steer), 30)
+        #wait_for_seconds(0.1)
+    robot.stop()
+    
+def old_highspeed_pid(hub, robot, cm, speed, start_angle):
     print('*******************')
     motor = Motor('F')
     motor.set_degrees_counted(0)
