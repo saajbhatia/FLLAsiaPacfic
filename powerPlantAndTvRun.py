a338ed75d656d838a58c735d2fa8990de31a13ae
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-06-03 11:10:00 -0700

    Update powerPlantAndTvRun.py

--- powerPlantAndTvRun.py
+++ powerPlantAndTvRun.py
@@ -134,36 +134,33 @@ DO NOT CHANGE
 # initAngle = 0
 def mission(hub, robot, flipper, back_flipper, flipperInit):
     #Go to pwr plant
-    abs_flip_turn(flipper, 55, 30, flipperInit)
-    highspeed_pid(hub, robot, 50.5, 80, 0)
-    abs_flip_turn(flipper, 0, 60, flipperInit)
+    abs_flip_turn(flipper, 60, 30, flipperInit)
+    highspeed_pid(hub, robot, 49, 80, 0)
+    abs_flip_turn(flipper, 0, 100, flipperInit)
 
     #Turn right 35, and go diagonal
-    abs_turning(hub, robot, 60, 10)
-    pid(hub, robot, 6, 50, 60)
+    abs_turning(hub, robot, 60, 30)
+    pid(hub, robot, 6.5, 50, 60)
 
     #Turn back to 0 b4 pushing thing down then push down
     abs_turning(hub, robot, 0, 30)
 
     #Go more B4 pushing down
-    pid(hub, robot, 8, 10, 0)
-    abs_flip_turn(flipper, 90, 30, flipperInit)
+    pid(hub, robot, 7.5, 50, 0)
+    abs_flip_turn(flipper, 90, 50, flipperInit)
 
     #Go HOME
-    highspeed_pid(hub, robot, 76, -80, 0)
+    highspeed_pid(hub, robot, 76, -100, 0)
 
     #Turn for TV
-    abs_turning(hub, robot, 90, 30)
+    abs_turning(hub, robot, 90, 50)
 
     #Tv mission go forward and come back
     pid(hub, robot, 33, 50, 90)
-    highspeed_pid(hub, robot, 23.75, -80, 90)
-    abs_flip_turn(flipper, 0, 30, flipperInit)
-
+    pid(hub, robot, 40, -100, 90)
+    abs_flip_turn(flipper, 0, 100, flipperInit)
+   
     print("Time for TV n Powerplant: " + str(time.time()-currentTime))
-
-
-
     car_windmill(hub, robot, flipper, back_flipper, flipperInit)
 
 
